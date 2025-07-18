206c4e527c1355c409f7bc5c34f572e760603ed7
GouriiNandanaa <150128982+GouriiNandanaa@users.noreply.github.com>
2025-07-18 17:23:34 +0530

    ji

---
+++ dom.js
@@ -0,0 +1,19 @@
+// examine the document object
+// console.dir(document)
+
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+
+document.title = 3232;
+console.log(document.all);
+
+let a = 10;
+console.log(a);
+var b = 100;
+
+console.log(window.a);
+console.log(window.b);  
+     
+    
+var items = document.getElementsByClassName("list-group-item");
