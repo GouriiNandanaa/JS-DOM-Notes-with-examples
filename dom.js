37604ddef27db7fdc0ebf424a3dc67b90c416243
GouriiNandanaa <150128982+GouriiNandanaa@users.noreply.github.com>
2025-07-22 11:15:10 +0530

    new

--- dom.js
+++ dom.js
@@ -20,7 +20,6 @@
 // // items[0].textContent = "jiji";
 // // items[0].style.color = "orange";
 
-
 // Array.from(items).forEach(element => {
 //     element.style.backgroundColor = "#f4f4f4"
 // });
@@ -28,8 +27,85 @@
 // var odd = document.querySelectorAll("li:nth-child(odd)");
 // odd.forEach((eachOdd)=> eachOdd.style.backgroundColor="blue");
 
+// var itemList = document.querySelector("#items");
+// console.log(itemList.parentNode);
 
-var itemList = document.querySelector("#items");
-console.log(itemList.parentNode);
+// console.log(itemList.childElementChild);
 
-console.log(itemList.childElementCount)
+// const newDiv = document.createElement("div");
+// newDiv.className = "hello";
+// newDiv.setAttribute("title", "HelloDiv");
+
+// const newDivText = document.createTextNode("hello all");
+
+// newDiv.appendChild(newDivText);
+
+// const container = document.querySelector("header .container");
+// const h1 = document.querySelector("header h1");
+// container.insertBefore(newDiv, h1);
+
+// event listeners
+// onclick is the previous way of doing the task
+
+// ideal way is to use the add event listener
+
+// var button = document.getElementById("button").addEventListener("click",buttonClick)
+
+// function buttonClick(e){
+//     document.getElementById('header-title').textContent = "changed"
+//     console.log(e);
+//     console.log(e.target)
+//     console.log(e.target.className)
+//     console.log(e.target.classList);
+
+//     console.log(e.clientX);//x co-ordinate of mouse click based on the whole html tag
+
+//     console.log(e.offsetX);//co ordinate based on the button
+
+//     console.log(e.shiftKey)
+//     // returns true is we click it by pressing shift key. ||ly ctrlKey, altKey are also there
+// }
+
+// types of events
+const events = [
+  "click",
+  "dblclick",
+  "mousedown",
+  "mouseup",
+  "mouseenter",
+  "mouseleave",
+  "mouseover",
+  "mouseout",
+  "mousemove",
+  "",
+  "",
+];
+
+// difference btw mouse over and mouseenter is that it will get triggered whenever i entered the region of that element even if it is blank.
+// mouse over will be triggered only if the calling will be in the contents
+
+const button = document.getElementById("button");
+const output = document.getElementById("output");
+// button.addEventListener("mouseup", runEvent);
+
+const box = document.getElementById("box");
+box.addEventListener("mousemove", runEvent);
+function runEvent(e) {
+  console.log("event type:" + e.type);
+  output.innerHTML = `<h3>Mouse X : ${e.offsetX}</h3>`;
+
+  box.style.backgroundColor = `rgb(${e.offsetX},${e.offsetY},40)`;
+
+  console.log(box.style.backgroundColor);
+}
+    
+
+// KEYDOWN  when the key is pressed
+// e.target.value  -> to live typing
+
+// KEYUP KEYPRESS are some other events
+
+// ////  input tag events
+//  focus, blur
+// cut paste
+// input
